75689d24824422a14fc852583731645f1ba906c7
OtherSuccesses <viskaslietuvai@gmail.com>
2018-02-15 00:02:48 -0500

    producing blanks but inquire issues

--- Word.js
+++ Word.js
@@ -1,27 +1,27 @@
-var Letter = require("./Letter");
+var Letter = require("./Letter.js");
 
 function Word(wordSelected){
 	this.wordSelected = wordSelected,
 	this.letters = [],
 	this.correct = 0,
 	this.lives=6,
-	this.correctGuess,
+	this.correctGuess = 0,
 	this.guessed = []
 }
 
 Word.prototype.getLetters = function(){
 	wordSelectedArr= this.wordSelected.split('');
 	for (var i = 0; i < wordSelectedArr.length; i++){
-		this.letters.push(new letter(wordSelectedArr[i]));
+		this.letters.push(new Letter(wordSelectedArr[i]));
 	} 
 }
 
 Word.prototype.displayWord = function(){
-	var displayWord = '';
-	for (var i = 0; this.letters.length; i++){
-		tempWord += this.letters[i].display();
+	var tempWord = [];
+	for (var i = 0; i < this.letters.length; i++){
+		tempWord.push(this.letters[i].display());
 	}
-	console.log(tempWord + "\n");
+	console.log(tempWord.join('') + "\n");
 }
 
 module.exports = Word;
--- hangman.js
+++ hangman.js
@@ -10,8 +10,61 @@ var game = {
 	
 
 	"gameStart" : function(){
+		console.log("This is the downgraded version of rugby hangman!\n")
 		var newWord= words[Math.floor(Math.random()*words.length)];
-		console.log(newWord);
+		game.currentWord = new Word(newWord);
+		console.log(game.currentWord);
+		game.currentWord.getLetters(game.currentWord.wordSelected);
+		console.log(game.currentWord);
+		game.currentWord.displayWord();
+		game.inquireStart();
+	},
+
+	"inquireStart": function(){
+		inquirer.prompt([{
+			message: 'Guess a letter',
+			name: 'letterGuess'
+		}]).then(function(reply){
+			if(game.alphabet.indexOf(reply.letterGuess > -1)){
+				console.log("Your letter is " + result.letterGuess);
+				var numberGuessed = game.letterCheck(reply.letterGuess);
+			}
+		})
+	},
+
+	"letterCheck": function(letter){
+		var letter = letter.toLowerCase();
+		var numberFound = 0;
+		if(game.alphabet.includes(letter) && game.currentWord.guessed.indexOf(letter) == -1){
+			for (var i = 0; i < game.currentWord.letters.length; i++){
+				if (game.currentWord.letters[i].letter == letter){
+					game.currentWord.letters[i].visible = true;
+					numberFound++
+					game.currentWord.correctGuess++;
+				}
+			}
+			if (numberFound == 0){
+				if(game.currentWord.guessed.includes(letter)){
+					console.log("You've already guessed that letter.")
+				}
+				else{
+					game.currentWord.lives--;
+					game.currentWord.guessed.push(letter);	
+				}
+				console.log("Letters used: " + game.currentWord.guessed);
+				console.log("You have " + game.currentWord.lives + " tries.\n");
+			}
+
+			else if(numberFound){
+				if (game.currentWord.guessed.includes(letter)){
+					console.log("You've alread tried that letter.")
+				}
+				else{
+					console.log("Correct! That letter does exist!");
+					game.currentWord.guessed.push(letter);
+				}
+			}
+		}
 	}
 }
 
